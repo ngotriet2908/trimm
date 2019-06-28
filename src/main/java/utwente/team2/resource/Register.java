54980f0abacee21c93b62ee1a9e93b792ee66fd8
s2093421 <o.khavrona@student.utwente.nl>
2019-06-28 23:10:04 +0200

    Version 4.9.6

--- src/main/java/utwente/team2/resource/Register.java
+++ src/main/java/utwente/team2/resource/Register.java
@@ -62,7 +62,7 @@ public class Register {
         // verifying/sanitizing input
         // check if the user already exists
         // email regex from TODO
-        if (username.matches("[a-zA-Z_]{2,}") &&
+        if (username.matches("[a-zA-Z0-9_]{2,20}") &&
                 firstName.matches("[a-zA-Z\\-\\s]+") &&
                 lastName.matches("[a-zA-Z\\-\\s]+") &&
                 email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])") &&
